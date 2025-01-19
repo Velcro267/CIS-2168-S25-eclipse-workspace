919a94bcf4f161694bad4238fa275fd224ddc38f
Velcro267 <165579641+Velcro267@users.noreply.github.com>
2025-01-18 22:45:50 -0500

    Update Rabbit.java

--- rabbit_hunt/src/Rabbit.java
+++ rabbit_hunt/src/Rabbit.java
@@ -9,16 +9,22 @@ public class Rabbit extends Animal {
     	//look all around for Fox
     	for(int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
     		if(look(i) == Model.FOX) {
-    			if(canMove(Model.turn(i, i))) 
-    				return Model.turn(i, i);
-    			else if(canMove(Model.turn))
-    			
-    			return Model.turn(i, 5);
+    			if(canMove(Model.turn(i,3))) {
+    				return Model.turn(i, 3);
+    			} else if(canMove(Model.turn(i, 5))) {
+    				return Model.turn(i, 5);
+    			} else if(canMove(Model.turn(i, 6))) {
+    				return Model.turn(i, 6);
+    			} else if(canMove(Model.turn(i, 2))){
+    				return Model.turn(i, 2);
+    			} else if(canMove(Model.turn(i, 1))) {
+    				return Model.turn(i, 1);
+    			} else {
+    				return Model.turn(i, 7);
+    			}
     		}
     		
-    		
     	}
-    	
     	
     	
 		return Model.STAY;
