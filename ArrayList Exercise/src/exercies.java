5ba939ac1769ab3f7783116edf9f15c165512021
Velcro267 <165579641+Velcro267@users.noreply.github.com>
2025-01-26 01:02:29 -0500

    Update exercies.java

--- ArrayList Exercise/src/exercies.java
+++ ArrayList Exercise/src/exercies.java
@@ -6,9 +6,35 @@ import java.util.Collections;
 public class exercies {
 
 	public static void main(String[] args) {
+		//Test Unique method
+		List<Integer> x = Arrays.asList(1, 25, 2, 5, 30, 19, 57, 2, 25);
+		System.out.println(unique(x));
+		
+		//Test All Multiples method
+		System.out.println(allMultiples(x, 5));
+		
+		//Test All Strings of Size method
+		List<String> y = Arrays.asList("I", "like", "to", "eat", "eat", "eat",""
+				+ "apples", "and", "bananas");
+		System.out.println(allStringsofSize(y, 3));
+		
+		//Test Is Permutation method
+		List<Integer> perm1 = Arrays.asList(1, 2, 4);
+		List<Integer> perm2 = Arrays.asList(2, 1, 4);
+		System.out.println(isPermutation(perm1, perm2));
+		
+		//Test String to List of Words method
+		String stuff = "Hello, world!";
+		System.out.println(stringToListOfWords(stuff));
+		
+		//Test Remove All Instances method 
+		List<Integer> z = Arrays.asList(1, 4, 5, 6, 5, 5, 2);
+		removeAllInstances(z, 5);
+		System.out.println(z);
+
 
 	}
-	
+
 	public static <E> boolean unique(List<E> list){
 		for(int i = 0; i < list.size(); i++) {
 			for(int j = 0; j < list.size(); j++) {
@@ -81,14 +107,15 @@ public class exercies {
 		while(list.contains(item)) {
 			for(int i = 0; i < list.size(); i++){
 				if(list.get(i).equals(item)) {
-					list.remove(i);
+					list.remove(item);
 				}
 			}
 			
 		}
 		
 		
-		
+	}
+	
 	
 	
 }
