2ab8b282e4be816434e3b89284b15dd3fe64b25e
Velcro267 <165579641+Velcro267@users.noreply.github.com>
2025-03-12 20:34:13 -0400

    erpdates

--- Recursion Exercise/src/recursion.java
+++ Recursion Exercise/src/recursion.java
@@ -3,16 +3,33 @@ import java.util.Arrays;
 public class recursion {
 
 	public static void main(String[] args) {
-		
+		//8 Queens Problem  
 		int [][] board = new int [8][8];
 		
-		
-		
-		if(solveBoard(board, 0)) {
+		if(solve8QBoard(board, 0)) {
 			printBoard(board);
 		} else {
 			System.out.println("No Solution.");
 		}
+		
+		
+		//Sudoku Problem
+		int[][] sudokuBoard = {
+				{5, 9, 0, 3, 0, 0, 8, 0, 2},
+				{1, 8, 0, 0, 0, 2, 9, 5, 3},
+				{7, 0, 0, 0, 0, 0, 0, 0, 1},
+				{2, 1, 0, 6, 0, 7, 3, 0, 0},
+				{6, 0, 0, 5, 4, 0, 1, 0, 0},
+				{0, 4, 9, 2, 0, 0, 0, 6, 5},
+				{3, 0, 8, 9, 0, 5, 4, 1, 0},
+				{0, 5, 1, 0, 6, 4, 2, 0, 0},
+				{0, 2, 0, 0, 0, 3, 5, 9, 0}, 
+				
+		};
+		
+		
+		
+		
 		
 	}
 	
@@ -29,7 +46,7 @@ public class recursion {
 	
 	
 	
-	public static boolean solveBoard(int [][]board, int col) {
+	public static boolean solve8QBoard(int [][]board, int col) {
 		//Base case: Off the board, or all queens placed 
 		if(col >=8) {
 			return true;
@@ -38,12 +55,12 @@ public class recursion {
 		
 		
 		for(int row = 0; row < 8; row++) {		//Try placing queen in each row of current column
-			if(isValid(board, row, col)) {
+			if(is8QValid(board, row, col)) {
 				board[row][col] = 1;			//Place queen
 				
 				
 				
-				if(solveBoard(board, col+1)) {	//Recursive Step: try placing next queen 
+				if(solve8QBoard(board, col+1)) {	//Recursive Step: try placing next queen 
 					return true;
 				}
 			}
@@ -65,7 +82,7 @@ public class recursion {
 	
 	
 	
-	public static boolean isValid(int [][]board, int row, int col) {
+	public static boolean is8QValid(int [][]board, int row, int col) {
 		//Check left side of row
 		for(int i = 0; i < col; i++) {		
 			if(board[row][i] == 1) {
